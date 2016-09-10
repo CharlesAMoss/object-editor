51444cf23ed003a26732a23c266dbb36d2b37d6a
CharlesAMoss <charlesamoss@yahoo.com>
2016-09-09 22:03:04 -0700

    add improved validation

--- app.php
+++ app.php
@@ -31,10 +31,13 @@ do {
     $avaibleClasses = implode(" ", $classList);
     echo "\n";
     $requestClass = "Welcome to Object Edit! "
-                    . "Chooose a class to instnce your "
+                    . "Chooose a \033[33mClass\033[0m to instnce your "
                     . "object :\r\n\033[33m{$avaibleClasses} \033[0m \r\n";
     fwrite(STDOUT, $requestClass );
     $requestedClass = trim(fgets(STDIN));
+    if (strlen($requestedClass) == 0) {
+      $requestedClass = $classList[0];
+    }
     $requestedClass = ucfirst($requestedClass);
 
     if (strlen($requestedClass) > 0 && in_array($requestedClass, $classList)) {
@@ -46,45 +49,45 @@ do {
       fwrite(STDOUT, $requestArguments);
       $classArguments = trim(fgets(STDIN));
       $args = explode(",", $classArguments);
-    } else {
-      die("The class \033[33m{$requestedClass}\033[0m could not be found!\r\n");
-    }
 
-    // instances the object and displays print it to terminal
-    $rc = new ReflectionClass($requestedClass);
-    $obj = $rc->newInstanceArgs( $args );
-    var_dump(get_object_vars($obj));
+      // instances the object and displays print it to terminal
+      $rc = new ReflectionClass($requestedClass);
+      $obj = $rc->newInstanceArgs($args);
+      var_dump(get_object_vars($obj));
+    } else {
+      echo "The class \033[33m{$requestedClass}\033[0m could not be found!\r\n";
+      $requestedClass = "";
+    }
 
   }
 
   //command prompt
-  echo "\n";
-  echo "\n";
+  echo "\n\n";
   fwrite(STDOUT,  "Type HELP for commands list.\r\n");
   $line = trim(fgets(STDIN));
 
 
-  if ($line == "help" || $line == "HELP") {
+  if ($line == "help" || $line == "HELP" || strlen($line) == 0) {
     //HELP command
     fwrite(STDERR, "HELP : view commands list \n"
                    . "SET < propertyname=value > : adds a property with a value to loaded object \n"
                    . "GET < propertyname > : retive the value of the property \n"
-                   . "GET * : Diplay Objects members and values \n"
+                   . "GET * : Diplay Object's properties and values \n"
                    . "EXIT : exit Object Edit \n");
 
-  } else if ((preg_match("/^SET\s[a-z][a-zA-Z1-9]*\=[a-zA-Z1-9]*$/", $line))) {
+  } else if ((preg_match("/^(SET|set)\s[a-z][a-zA-Z1-9]*\=[a-zA-Z1-9]*$/", $line))) {
     //SET propertyname=value command
     $propStr = substr($line, 4);
       if ((preg_match("/^[a-z][a-zA-Z1-9]*\=[a-zA-Z1-9]*$/", $propStr))) {
         $newProp = explode("=", $propStr);
         $obj->$newProp[0] = $newProp[1];
       }
-  } else if ((preg_match("/^GET\s[a-z][a-zA-Z1-9]*$/", $line))) {
+  } else if ((preg_match("/^(GET|get)\s[a-z][a-zA-Z1-9]*$/", $line))) {
     //GET propertyname command
       $propStr = substr($line, 4);
       $getProp = "The value of property {$propStr} is {$obj->$propStr} \r\n";
       echo $getProp;
-  } else if ((preg_match("/^GET\s\*$/", $line))) {
+  } else if ((preg_match("/^(GET|get)\s\*$/", $line))) {
     //GET * command
       var_dump(get_object_vars($obj));
   } else if ($line == "exit" || $line == "EXIT") {
@@ -93,10 +96,10 @@ do {
       $inUse = false;
       exit(0);
   } else {
-    // no condtions met, program ends with error message
+    // no condtions met, error message
       echo "I am sorry, but that is not a vaild command. \r\n";
   }
 
-} while ($inUse === true);
+} while ($inUse == true);
 
 ?>
